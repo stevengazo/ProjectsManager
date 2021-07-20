9bd436b6cb4927fbdfd913d45e5bb59f6c116f4d
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-07-19 21:01:05 -0600

    Uso de Scaffolding para generar vistas y conexión con DB

---
+++ ProjectsControl/Controllers/AsistancesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectsControl.Models;
+
+namespace ProjectsControl.Controllers
+{
+    public class AsistancesController : Controller
+    {
+        private readonly DBProjectContext _context;
+
+        public AsistancesController(DBProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Asistances
+        public async Task<IActionResult> Index()
+        {
+            var dBProjectContext = _context.Asistances.Include(a => a.Employee).Include(a => a.Project);
+            return View(await dBProjectContext.ToListAsync());
+        }
+
+        // GET: Asistances/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asistance = await _context.Asistances
+                .Include(a => a.Employee)
+                .Include(a => a.Project)
+                .FirstOrDefaultAsync(m => m.AsistanceId == id);
+            if (asistance == null)
+            {
+                return NotFound();
+            }
+
+            return View(asistance);
+        }
+
+        // GET: Asistances/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId");
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId");
+            return View();
+        }
+
+        // POST: Asistances/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AsistanceId,DateOfBegin,DateOfEnd,EmployeeId,ProjectId")] Asistance asistance)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(asistance);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", asistance.EmployeeId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId", asistance.ProjectId);
+            return View(asistance);
+        }
+
+        // GET: Asistances/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asistance = await _context.Asistances.FindAsync(id);
+            if (asistance == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", asistance.EmployeeId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId", asistance.ProjectId);
+            return View(asistance);
+        }
+
+        // POST: Asistances/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("AsistanceId,DateOfBegin,DateOfEnd,EmployeeId,ProjectId")] Asistance asistance)
+        {
+            if (id != asistance.AsistanceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(asistance);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AsistanceExists(asistance.AsistanceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", asistance.EmployeeId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId", asistance.ProjectId);
+            return View(asistance);
+        }
+
+        // GET: Asistances/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asistance = await _context.Asistances
+                .Include(a => a.Employee)
+                .Include(a => a.Project)
+                .FirstOrDefaultAsync(m => m.AsistanceId == id);
+            if (asistance == null)
+            {
+                return NotFound();
+            }
+
+            return View(asistance);
+        }
+
+        // POST: Asistances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var asistance = await _context.Asistances.FindAsync(id);
+            _context.Asistances.Remove(asistance);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AsistanceExists(string id)
+        {
+            return _context.Asistances.Any(e => e.AsistanceId == id);
+        }
+    }
+}
---
+++ ProjectsControl/Controllers/ExtraHoursController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectsControl.Models;
+
+namespace ProjectsControl.Controllers
+{
+    public class ExtraHoursController : Controller
+    {
+        private readonly DBProjectContext _context;
+
+        public ExtraHoursController(DBProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ExtraHours
+        public async Task<IActionResult> Index()
+        {
+            var dBProjectContext = _context.ExtraHours.Include(e => e.Asistance).Include(e => e.Employee);
+            return View(await dBProjectContext.ToListAsync());
+        }
+
+        // GET: ExtraHours/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var extraHour = await _context.ExtraHours
+                .Include(e => e.Asistance)
+                .Include(e => e.Employee)
+                .FirstOrDefaultAsync(m => m.ExtraHourId == id);
+            if (extraHour == null)
+            {
+                return NotFound();
+            }
+
+            return View(extraHour);
+        }
+
+        // GET: ExtraHours/Create
+        public IActionResult Create()
+        {
+            ViewData["AsistanceId"] = new SelectList(_context.Asistances, "AsistanceId", "AsistanceId");
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId");
+            return View();
+        }
+
+        // POST: ExtraHours/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ExtraHourId,BeginTime,EndTime,TypeOfHour,Reason,Notes,IsPaid,EmployeeId,AsistanceId")] ExtraHour extraHour)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(extraHour);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AsistanceId"] = new SelectList(_context.Asistances, "AsistanceId", "AsistanceId", extraHour.AsistanceId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", extraHour.EmployeeId);
+            return View(extraHour);
+        }
+
+        // GET: ExtraHours/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var extraHour = await _context.ExtraHours.FindAsync(id);
+            if (extraHour == null)
+            {
+                return NotFound();
+            }
+            ViewData["AsistanceId"] = new SelectList(_context.Asistances, "AsistanceId", "AsistanceId", extraHour.AsistanceId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", extraHour.EmployeeId);
+            return View(extraHour);
+        }
+
+        // POST: ExtraHours/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ExtraHourId,BeginTime,EndTime,TypeOfHour,Reason,Notes,IsPaid,EmployeeId,AsistanceId")] ExtraHour extraHour)
+        {
+            if (id != extraHour.ExtraHourId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(extraHour);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExtraHourExists(extraHour.ExtraHourId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AsistanceId"] = new SelectList(_context.Asistances, "AsistanceId", "AsistanceId", extraHour.AsistanceId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", extraHour.EmployeeId);
+            return View(extraHour);
+        }
+
+        // GET: ExtraHours/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var extraHour = await _context.ExtraHours
+                .Include(e => e.Asistance)
+                .Include(e => e.Employee)
+                .FirstOrDefaultAsync(m => m.ExtraHourId == id);
+            if (extraHour == null)
+            {
+                return NotFound();
+            }
+
+            return View(extraHour);
+        }
+
+        // POST: ExtraHours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var extraHour = await _context.ExtraHours.FindAsync(id);
+            _context.ExtraHours.Remove(extraHour);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExtraHourExists(string id)
+        {
+            return _context.ExtraHours.Any(e => e.ExtraHourId == id);
+        }
+    }
+}
