7ec5c1b50d27ba45787a26bc6e11706a15a015f9
Steven Gazo M <sfabricio27@live.com>
2021-08-18 11:02:10 -0600

    clase notas para los proyectos. Falta llaves y nueva migracion

---
+++ ProjectsControl/Models/Notes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectsControl.Models
+{
+    public class Notes
+    {
+        [Key]
+        [Required]
+        public string NotesId { get; set; }
+        [Required]
+        public string Author { get; set; }
+        public string DateOfCreation { get; set; }
+        [Required]
+        [MaxLength(120)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLength(340)]
+        public string NoteDescription { get; set; }
+    }
+}
