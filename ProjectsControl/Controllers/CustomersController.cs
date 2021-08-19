6f83acbf7f3b343a6744bbffad54f913884f56e1
Steven Gazo M <sfabricio27@live.com>
2021-08-19 12:27:09 -0600

    cambios varios

--- ProjectsControl/Controllers/CustomersController.cs
+++ ProjectsControl/Controllers/CustomersController.cs
@@ -53,7 +53,7 @@ namespace ProjectsControl.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CustomerId,ProjectName,Sector")] Customer customer)
+        public async Task<IActionResult> Create([Bind("CustomerId,Name,Sector")] Customer customer)
         {
             if (ModelState.IsValid)
             {
