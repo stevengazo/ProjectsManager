8444dbb2be495acaeb48c4d93b04bf48e5efd989
Steven Gazo M <sfabricio27@live.com>
2021-08-19 12:53:23 -0600

    inicio de cambios en el proyecto, creacion de entidades

---
+++ ProjectsControl/Models/Action.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectsControl.Models
+{
+    public class Action
+    {
+        [Key]
+        [Required]
+        public string ActionId { get; set; }
+        [Required]
+        [MinLength(10)]
+        public string Title { get; set; }
+        [Required]
+        public DateTime DateOfCreation { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string TypeOfAction { get; set; }
+        [Required]
+        [MaxLength(340)]
+        public string Description { get; set; }
+        public string IsActive { get; set; }
+
+      
+    }
+}
---
+++ ProjectsControl/Models/Offer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectsControl.Models
+{
+    public class Offer
+    {
+        [Key]        
+        [Required(ErrorMessage= "El Id de la oferta no puede ser nulo")]
+        public string OfferId { get; set; }
+
+        public int NumberOfOffer { get; set; }
+        [Required(ErrorMessage ="El Titulo de la oferta es requerido")]
+        public string Title { get; set; }
+        [Required(ErrorMessage ="El tipo de oferta es requerido")]
+        [MinLength(8,ErrorMessage ="La descripcion es muy corta")]
+        [MaxLength(100,ErrorMessage ="La descripcion es muy larga")]
+        public string Type { get; set; }
+        [Required(ErrorMessage ="Monto del proyecto requerido Requerida")]
+        public double Amount { get; set; }        
+        [Required(ErrorMessage = "Descripcion Requerida")]
+        public string Description { get; set; }
+        [Required(ErrorMessage ="Cantidadd de Dias requerida")]
+        public int QuantityOfDays { get; set; }
+        [Required(ErrorMessage ="Cantidad de Personas Requerida")]
+        public int QuantityOfEmployees { get; set; }
+        [Required(ErrorMessage ="Introduzca el nombre del autor")]
+        public string Author { get; set; }
+        public DateTime CreationDate { get; set; }
+        
+    }
+}
