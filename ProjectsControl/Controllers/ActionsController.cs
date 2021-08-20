7a463fcd1740b270f87b2a28f039174bccc7cf5d
Steven Gazo M <sfabricio27@live.com>
2021-08-20 14:07:13 -0600

    Construccion de nuevas vistas basicas con scalfonding

---
+++ ProjectsControl/Controllers/ActionsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectsControl.Models;
+using Action = ProjectsControl.Models.Action;
+
+namespace ProjectsControl.Controllers
+{
+    public class ActionsController : Controller
+    {
+        private readonly DBProjectContext _context;
+
+        public ActionsController(DBProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Actions
+        public async Task<IActionResult> Index()
+        {
+            var dBProjectContext = _context.Actions.Include(a => a.Employee);
+            return View(await dBProjectContext.ToListAsync());
+        }
+
+        // GET: Actions/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var action = await _context.Actions
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.ActionId == id);
+            if (action == null)
+            {
+                return NotFound();
+            }
+
+            return View(action);
+        }
+
+        // GET: Actions/Create
+        public IActionResult Create()
+        {
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId");
+            return View();
+        }
+
+        // POST: Actions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ActionId,Title,DateOfCreation,Author,TypeOfAction,Description,IsActive,EmployeeId")]Action oaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(oaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", oaction.EmployeeId);
+            return View(oaction);
+        }
+
+        // GET: Actions/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var action = await _context.Actions.FindAsync(id);
+            if (action == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", action.EmployeeId);
+            return View(action);
+        }
+
+        // POST: Actions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ActionId,Title,DateOfCreation,Author,TypeOfAction,Description,IsActive,EmployeeId")] Action oaction)
+        {
+            if (id != oaction.ActionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(oaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActionExists(oaction.ActionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "EmployeeId", "EmployeeId", oaction.EmployeeId);
+            return View(oaction);
+        }
+
+        // GET: Actions/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var action = await _context.Actions
+                .Include(a => a.Employee)
+                .FirstOrDefaultAsync(m => m.ActionId == id);
+            if (action == null)
+            {
+                return NotFound();
+            }
+
+            return View(action);
+        }
+
+        // POST: Actions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var action = await _context.Actions.FindAsync(id);
+            _context.Actions.Remove(action);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActionExists(string id)
+        {
+            return _context.Actions.Any(e => e.ActionId == id);
+        }
+    }
+}
---
+++ ProjectsControl/Controllers/NotesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectsControl.Models;
+
+namespace ProjectsControl.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly DBProjectContext _context;
+
+        public NotesController(DBProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notes
+        public async Task<IActionResult> Index()
+        {
+            var dBProjectContext = _context.Notes.Include(n => n.Project);
+            return View(await dBProjectContext.ToListAsync());
+        }
+
+        // GET: Notes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notes = await _context.Notes
+                .Include(n => n.Project)
+                .FirstOrDefaultAsync(m => m.NotesId == id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+
+            return View(notes);
+        }
+
+        // GET: Notes/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId");
+            return View();
+        }
+
+        // POST: Notes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NotesId,Author,DateOfCreation,Title,NoteDescription,ProjectId")] Notes notes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(notes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId", notes.ProjectId);
+            return View(notes);
+        }
+
+        // GET: Notes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notes = await _context.Notes.FindAsync(id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId", notes.ProjectId);
+            return View(notes);
+        }
+
+        // POST: Notes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("NotesId,Author,DateOfCreation,Title,NoteDescription,ProjectId")] Notes notes)
+        {
+            if (id != notes.NotesId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotesExists(notes.NotesId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "ProjectId", notes.ProjectId);
+            return View(notes);
+        }
+
+        // GET: Notes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notes = await _context.Notes
+                .Include(n => n.Project)
+                .FirstOrDefaultAsync(m => m.NotesId == id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+
+            return View(notes);
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var notes = await _context.Notes.FindAsync(id);
+            _context.Notes.Remove(notes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotesExists(string id)
+        {
+            return _context.Notes.Any(e => e.NotesId == id);
+        }
+    }
+}
