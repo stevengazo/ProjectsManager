39285d1b06596c6c883049c80b0e0a09fba6e0af
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2022-07-28 17:04:52 -0600

    inicio pagina de estadisticas

---
+++ ProjectsControl/Controllers/StadisticController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChartJSCore.Helpers;
+using ChartJSCore.Models;
+using ChartJSCore.Models.ChartJSCore.Models;
+using ChartJSCore.Models.Polar;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ProjectsControl.Models;
+
+
+namespace ProjectsControl.Controllers
+{
+    public class StadisticController : Controller
+    {
+        private readonly DBProjectContext _context;
+
+        public StadisticController(DBProjectContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: StadisticController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Ventas()
+        {
+            return View();
+        }
+
+    
+    }
+}
