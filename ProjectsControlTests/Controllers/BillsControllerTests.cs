e1b1873332ca88318d82c48912cc2b8e5e48699d
Steven Gazo M <steven.gazo@hotmail.com>
2022-11-22 21:33:22 -0600

    docker compose

---
+++ ProjectsControlTests/Controllers/BillsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectsControl.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsControl.Controllers.Tests
+{
+    [TestClass()]
+    public class BillsControllerTests
+    {
+        [TestMethod()]
+        public void BillsControllerTest()
+        {
+            
+        }
+    }
+}
